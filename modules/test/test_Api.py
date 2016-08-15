1218fb4e3d515f749f550f97ca3aad98aed79e94
David Lomelin <david.lomelin@gmail.com>
2016-08-14 21:02:19 -0700

    Linted the unittest file

--- modules/test/test_Api.py
+++ modules/test/test_Api.py
@@ -1,55 +1,69 @@
+'''.'''  #pylint: disable=invalid-name
+
 import unittest
 from HboMovieData.modules.Api import ApiUrl, ImdbApi, MovieDataApi
 
 
 class TestApiUrl(unittest.TestCase):
+    ''' Test the ApiUrl class '''
+
     def setUp(self):
-        self.apiObj = ApiUrl()
+        ''' Runs this before all tests '''
+        self.api = ApiUrl()
 
     def test_get_url(self):
+        ''' Test the get_url method using no arguments '''
         self.assertEqual(
-            self.apiObj.get_url(),
+            self.api.get_url(),
             'http://mySite.com/',
         )
 
     def test_get_url_with_path(self):
-        class NewApi(ApiUrl):
+        ''' Test the get_url method using 1 argument '''
+        class NewApi(ApiUrl):  # pylint: disable=too-few-public-methods
+            ''' Testing child class '''
             PATH = '/api/%s/names/'
 
-        apiObj = NewApi()
+        api = NewApi()
 
         self.assertEqual(
-            apiObj.get_url('Hello'),
+            api.get_url('Hello'),
             'http://mySite.com/api/Hello/names/',
         )
 
     def test_get_url_with_paths(self):
-        class NewApi(ApiUrl):
+        ''' Test the get_url method using 2 arguments of mixed type '''
+        class NewApi(ApiUrl):  # pylint: disable=too-few-public-methods
+            ''' Testing child class '''
             PATH = '/api/%s/names/%s/'
 
-        apiObj = NewApi()
+        api = NewApi()
 
         self.assertEqual(
-            apiObj.get_url('Hello', 3),
+            api.get_url('Hello', 3),
             'http://mySite.com/api/Hello/names/3/',
         )
 
 
 class TestMovieDataApi(unittest.TestCase):
+    ''' Test the MovieDataApi class '''
+
     def setUp(self):
-        self.apiObj = MovieDataApi()
+        ''' Runs this before all tests '''
+        self.api = MovieDataApi()
 
     def test_get_movie_data(self):
+        ''' Test the get_movie_data method using imdb_id and movie arguments '''
         imdb_id = 'tt0090605'
         movie = 'Aliens'
 
-        data = self.apiObj.get_movie_data(imdb_id=imdb_id)
-        self.__validateAliensData(data, imdb_id, movie)
+        data = self.api.get_movie_data(imdb_id=imdb_id)
+        self.__validate_aliens_data(data, imdb_id, movie)
 
-        data = self.apiObj.get_movie_data(movie=movie)
-        self.__validateAliensData(data, imdb_id, movie)
+        data = self.api.get_movie_data(movie=movie)
+        self.__validate_aliens_data(data, imdb_id, movie)
 
-    def __validateAliensData(self, data, imdb_id, movie):
+    def __validate_aliens_data(self, data, imdb_id, movie):
         self.assertEqual(
             data['Title'],
             movie,
@@ -65,12 +79,16 @@ class TestMovieDataApi(unittest.TestCase):
 
 
 class TestImdbApi(unittest.TestCase):
+    ''' Test the ImdbApi class '''
+
     def setUp(self):
-        self.apiObj = ImdbApi()
+        ''' Runs this before all tests '''
+        self.api = ImdbApi()
 
     def test_get_url(self):
+        ''' Test get_url method with standard arguments '''
         self.assertEqual(
-            self.apiObj.get_url('tt0090605'),
+            self.api.get_url('tt0090605'),
             'http://www.imdb.com/title/tt0090605/',
         )
 
